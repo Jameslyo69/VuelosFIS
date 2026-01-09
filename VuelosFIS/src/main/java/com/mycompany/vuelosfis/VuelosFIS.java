e09f5092258d45308e904370c32892e76b4db3c3
Jameslyo69 <cainojames45@gmail.com>
2026-01-09 17:37:47 -0500

    Initial NetBeans project (base structure)

---
+++ VuelosFIS/src/main/java/com/mycompany/vuelosfis/VuelosFIS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vuelosfis;
+
+/**
+ *
+ * @author caino
+ */
+public class VuelosFIS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
